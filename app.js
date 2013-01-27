833c87554fef12ec0215b04b3d0662a83509dd63
tim walker <tim.twalker@gmail.com>
2013-01-26 17:18:52 -0800

    add nib; tweak reload

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var express = require('express'),
 	routes = require('./routes'),
 	eggs = require('./routes/eggs'),
 	http = require('http'),
-	path = require('path');
+	path = require('path'),
+	stylus = require('stylus'),
+	nib = require('nib');
 
 var app = express();
-
 
 app
 	.set('port', process.env.PORT || 3000)
@@ -15,7 +16,10 @@ app
 	.use(express.logger('dev'))
 	.use(express.bodyParser())
 	.use(app.router)
-	.use(require('stylus').middleware(__dirname + '/public'))
+	.use(stylus.middleware({
+		src: path.join(__dirname, 'public'),
+		compile: compileCss
+	}))
 	.use(express.static(path.join(__dirname, 'public')));
 
 app.configure('development', function(){
@@ -25,11 +29,18 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/api/eggs', eggs.list);
 
+function compileCss(str, path) {
+	return stylus(str)
+		.set('filename', path)
+		.set('compress', true)
+		.use(nib());
+}
+
 http.createServer(app).listen(app.get('port'), function(){
 	console.log("Express server listening on port " + app.get('port'));
 });
 
 // live reload
 var livereload = require('livereload');
-var server = livereload.createServer({exts: ['styl', 'css', 'js']});
-server.watch(__dirname + "/public");
+var server = livereload.createServer({exts: ['styl', 'js', 'mustache']});
+server.watch(path.join(__dirname, 'public'));
