0ee17499e0e0c3e8e0ce636b1151ffd5c44f5a6e
tim walker <tim.twalker@gmail.com>
2013-02-10 16:33:34 -0800

    switch to grunt-contrib-livereload--partial work on #2

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,16 @@
 module.exports = function(grunt) {
 
-	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-
-		watch: {
-			reload: {
-				files: [
-					'views/*.jade',
-					'public/css/*.css',
-					'public/js/*.js',
-					'public/img/*.{png,jpg,jpeg}'
-
-					// '**/*.html',
-					// '**/styles/*.css',
-					// '**/scripts/*.js',
-					// '**/images/*.{png,jpg,jpeg}'
-				],
-				tasks: 'tinylr-reload'
+		regarde: {
+			css: {
+				files: 'public/css/*',
+				tasks: ['livereload']
 			}
-		}
-		
+		}		
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('tiny-lr');
-
-	grunt.registerTask('reload', ['tinylr-start', 'watch']);
-	grunt.registerTask('default', ['reload']);
+	grunt.loadNpmTasks('grunt-regarde');
+	grunt.loadNpmTasks('grunt-contrib-livereload');
+	grunt.registerTask('default', ['livereload-start', 'regarde']);
 };
