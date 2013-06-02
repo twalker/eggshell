e4a71d9202488a60cd0e6abb9af4f6d2d7279ba5
tim walker <tim.twalker@gmail.com>
2013-06-01 20:53:42 -0700

    add mixer utility

---
+++ public/js/src/mixer.js
@@ -0,0 +1,67 @@
+/**
+ * Mixer of mixins. A utility to copy functionality from mixins to objects.
+ */
+define(['underscore'], function(lodash){
+	// heavily inspired by: https://github.com/onsi/cocktail
+	var mergeIn = function mergeIn(klass){
+		var mixins = lodash(arguments).toArray().rest().flatten().value();
+		var collisions = {};
+
+		lodash(mixins).each(function(mixin) {
+			lodash(mixin).each(function(value, key) {
+				if (lodash.isFunction(value)) {
+					if (klass.prototype[key]) {
+						collisions[key] = collisions[key] || [klass.prototype[key]];
+						collisions[key].push(value);
+					}
+					klass.prototype[key] = value;
+				} else if (lodash.isObject(value)) {
+					klass.prototype[key] = lodash.extend({}, value, klass.prototype[key] || {});
+				}
+			});
+		});
+
+		lodash(collisions).each(function(propertyValues, propertyName) {
+			klass.prototype[propertyName] = function() {
+				var that = this,
+						args = arguments,
+						returnValue;
+
+				lodash(propertyValues).each(function(value) {
+					var returnedValue = lodash.isFunction(value) ? value.apply(that, args) : value;
+					returnValue = (returnedValue === undefined ? returnValue : returnedValue);
+				});
+
+				return returnValue;
+			}
+		});
+
+	};
+
+	return {
+		/**
+		 * Non-destructively copies members to a destination object from source objects.
+		 * A simple proxy to lodash.defaults.
+		 * @example
+		 * mixer.mixin(MyModel.prototype, modelMixin1, modelMixin2);
+		 */
+		mixin: lodash.defaults,
+		/**
+		 * Destructively copies members to a destination object from source objects.
+		 * A simple proxy to lodash.extend.
+		 * @example
+		 * mixer.extend(MyModel.prototype, modelMixin1, modelMixin2);
+		 */
+		extend: lodash.extend,
+		/**
+		 * Copies members from mixins to a Constructor (klass), merging colliding
+		 * members.
+		 * Most useful when a mixin has initialize, event, or defaults.
+		 * @example
+		 * mixer.merge(MyModel, {
+		 *	initialize: function(){return "called after MyModel.initialize"}
+		 * }, modelMixin2);
+		 */
+		merge: mergeIn
+	};
+});
---
+++ public/js/test/mixer.js
@@ -0,0 +1,141 @@
+require([
+	'mocha',
+	'chai',
+	'sinon',
+	'backbone',
+	'mixer'
+], function(mocha, chai, sinon, Backbone, mixer){
+
+	// setup
+	var assert = chai.assert;
+
+	mocha.setup('bdd');
+
+	describe('mixer', function(){
+		var noop = function noop(){};
+
+		describe('.mixin(dest [, source1, source2, …])', function(){
+			var Mod, modMixin;
+
+			beforeEach(function(){
+				Mod = Backbone.Model.extend({
+					defaults: { first: 'sid'}
+				});
+
+				modMixin = {
+					defaults: {last: 'vicious'},
+					playBass: noop
+				};
+
+				mixer.mixin(Mod.prototype, modMixin, {rebel: noop});
+			});
+
+
+			it('should copy methods/properties to a destination object from sources', function(){
+				assert.isDefined(Mod.prototype.playBass);
+				assert.isDefined(Mod.prototype.rebel);
+			});
+
+			it('should not overwrite existing members', function(){
+				assert.deepEqual(Mod.prototype.defaults, {first:'sid'});
+
+				var bestPunk = {};
+				mixer.mixin(bestPunk, {band: 'sex pistols'}, {band: 'the clash'});
+				assert.equal(bestPunk.band, 'sex pistols');
+			});
+
+		});
+
+		describe('.extend(dest [, source1, source2, …])', function(){
+			var Mod, modMixin;
+
+			beforeEach(function(){
+				Mod = Backbone.Model.extend({
+					defaults: {first: 'sid'}
+				});
+
+				modMixin = {
+					defaults: {last: 'vicious'},
+					playBass: noop
+				};
+
+				mixer.extend(Mod.prototype, modMixin, {rebel: noop});
+			});
+
+			it('should copy methods/properties to a destination object from sources', function(){
+				assert.isDefined(Mod.prototype.playBass);
+				assert.isDefined(Mod.prototype.rebel);
+			});
+
+			it('should overwrite existing members', function(){
+				assert.deepEqual(Mod.prototype.defaults, {last:'vicious'});
+
+				var bestPunk = {};
+				mixer.extend(bestPunk, {band: 'sex pistols'}, {band: 'the clash'});
+				assert.equal(bestPunk.band, 'the clash');
+			});
+		});
+
+		describe('.merge(klass [, source1, source2, …])', function(){
+			var Mod, modMixin;
+			var initSpy, mixinInitSpy;
+
+			beforeEach(function(){
+				initSpy = sinon.spy();
+				mixinInitSpy = sinon.spy();
+
+				Mod = Backbone.Model.extend({
+					defaults: {first: 'sid'},
+					initialize: initSpy
+				});
+
+				modMixin = {
+					defaults: {last: 'vicious'},
+					playBass: noop,
+					initialize: mixinInitSpy
+				};
+
+				mixer.merge(Mod, modMixin, {rebel: noop});
+			});
+
+			it('should merge methods/properties to a destination object from sources', function(){
+				assert.isDefined(Mod.prototype.playBass);
+				assert.isDefined(Mod.prototype.rebel);
+			});
+
+			it('should merge colliding/existing members', function(){
+				assert.deepEqual(Mod.prototype.defaults, {first:'sid', last: 'vicious'});
+				var options = {rock: 'hard'};
+				new Mod({}, options);
+
+				assert.isTrue(initSpy.called);
+				assert.isTrue(mixinInitSpy.called);
+				assert.isTrue(initSpy.calledWith({}, options));
+				assert.isTrue(mixinInitSpy.calledWith({}, options));
+			});
+
+			it('should merge N collisions in their source order', function(){
+				var thirdSpy = sinon.spy();
+
+				mixer.merge(Mod, {initialize: thirdSpy});
+
+				var m = new Mod({});
+
+				assert.isTrue(initSpy.called);
+				assert.isTrue(initSpy.calledBefore(mixinInitSpy));
+
+				assert(initSpy.calledOnce);
+				assert(initSpy.calledOn(m));
+
+				assert(mixinInitSpy.called);
+				assert(mixinInitSpy.calledBefore(thirdSpy));
+
+				assert(thirdSpy.called);
+				assert(thirdSpy.calledAfter(mixinInitSpy));
+			});
+		});
+	});
+
+	// Start runner
+	mocha.run();
+});
