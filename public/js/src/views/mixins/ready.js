0770a7843a6a73925bce2d980f40296230e6d675
tim walker <tim.twalker@gmail.com>
2013-07-25 08:38:26 -0700

    add ready view mixin

---
+++ public/js/src/views/mixins/ready.js
@@ -0,0 +1,25 @@
+/**
+* View.ready indicates when a view has rendered, returning a promise.
+*
+* Needs to be patched into a view's prototype due to the initialize and render methods.
+* e.g. mixer.patch(View2.prototype, ready);
+**/
+define(function(require){
+	var jQuery = require('jquery');
+
+	return {
+		initialize: function(){
+			this._dfrReady = new jQuery.Deferred();
+		},
+
+		ready: function(){
+			return this._dfrReady.promise();
+		},
+
+		render: function(){
+			this._dfrReady.resolve(this);
+			return this;
+		}
+	};
+
+});
