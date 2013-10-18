4c44a4cf0d8ec826f8a313b48277faf6e6f9a7bd
tim walker <tim.twalker@gmail.com>
2013-10-17 22:54:26 -0700

    add a msg

--- public/js/src/xhr-worker.js
+++ public/js/src/xhr-worker.js
@@ -1,19 +1,38 @@
+/**
+ * A synchronous xhr webworker.
+ *
+ * TODO:
+ * - error message
+ * - add CORS support for IE: http://www.kendoui.com/blogs/teamblog/posts/11-10-03/using_cors_with_all_modern_browsers.aspx
+ * - support json, text, formdata, passthrough?
+ * - support buffer for dataURL
+ */
+
 //postMessage("I\'m working before postMessage(\'ali\').");
 /*
 require(['/bower_components/reqwest/reqwest'], function(req){
   postMessage('HEEEELLLOOO!')
 });
 */
+//importScripts('/js/vendor/lodash/js/lodash.js');
 self.addEventListener('message', function(e) {
   postMessage("Hi " + e.data.name);
-  /*
+  var options = {
+    url: '/',
+    method: 'GET',
+    mime: 'application/json', //YAGNI?
+    body: {}
+  };
+  for(var p in e.data) options[p] = e.data[p];
+
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', event.data, false);
-  xhr.send();
-  postMessage(xhr.responseText);
-  */
-}, false);
+  // synchronous xhr since webworker has a thread
+  xhr.open(options.method, options.url, false);
+  xhr.setRequestHeader('Content-Type', options.mime);
+  xhr.send(JSON.stringify(options.body));
+  postMessage(JSON.parse(xhr.responseText));
 
-onmessage = function(e){ postMessage('hello')}
+  self.close();
+});
 
-self.close();
+
--- public/js/test/xhr-worker.js
+++ public/js/test/xhr-worker.js
@@ -8,20 +8,20 @@ require(['mocha', 'chai'
 
   describe("xhr worker", function() {
 
-    it("should do stuff", function(){
+    it("should creat an xhr thread", function(done){
       var worker = new Worker('/js/src/xhr-worker.js');
       worker.addEventListener('message', function(e){
         console.log(e)
-        console.log("Worker said: " + e.data);
-        // not firing
-      }, false);
-
-      worker.postMessage({name: "tim"});
+        console.log("Worker returned: " + e.data);
+        done()
+      });
+      worker.postMessage({url: '/api/eggs', method: 'GET'});
+      //worker.postMessage({url: '/api/eggs', method: 'POST', body: {'name':'black', cracked: true}});
       assert.isObject(worker);
 
     });
 
-    it.skip('should provide an abstracted wrapper around a worker that returns a promise', function(){
+    it.skip('could provide an abstracted wrapper around a worker that returns a promise', function(){
       var worker = new xhrWorker({url: 'foo/bar', method: 'get'});
       worker.done(function(jmsg){
 
