c8738cc0ba12d1bc3c87090fd83bcfd031c72c0d
tim walker <tim.twalker@gmail.com>
2014-06-15 18:10:12 -0700

    silly copy tweaks

--- public/js/src/backbone-promises.js
+++ public/js/src/backbone-promises.js
@@ -1,17 +1,17 @@
 /**
- * backbone-promises provides ES6 Promises for sync operations.
+ * backbone-promises returns ES6 Promises for Backbone.sync operations.
  * @return {Object} monkey-patched Backbone.
  *
  */
 define(['backbone-lib', 'es6-promise'], function(Backbone, Promise){
 
-  // Backbone.ajax to use native ES6 promises for ajax calls
+  // Backbone.ajax to use native ES6 promises for ajax calls insead of jQuery.Deferreds
   var origAjax = Backbone.ajax;
   Backbone.ajax = function ajax(){
     return Promise.resolve(origAjax.apply(Backbone.$, arguments));
   }
 
-  // Backbone.sync to resolve with model/collection as the settlement argument.
+  // Backbone.sync to resolve with models/collections as the settlement argument.
   var origSync = Backbone.sync;
   Backbone.sync = function sync(method, model, options){
     return Promise.resolve(origSync(method, model, options))
@@ -22,7 +22,7 @@ define(['backbone-lib', 'es6-promise'], function(Backbone, Promise){
   var origSave = Backbone.Model.prototype.save;
   Backbone.Model.prototype.save = function save(){
     var xhr = origSave.apply(this, arguments);
-    // By this point, orgSave has usually validated the model (emitting 'invalid' event),
+    // By this point, orgSave has typically validated the model (emitting 'invalid' event),
     // and sync would have caught xhr errors (rejecting with xhr, and emitting 'error' event).
     // Rejecting with Model error instead of false.
     return (xhr !== false) ? xhr : Promise.reject(new Error('ModelError'));
@@ -30,7 +30,7 @@ define(['backbone-lib', 'es6-promise'], function(Backbone, Promise){
 
   // Model.prototype.destroy to reject on errors, NOT return false.
   var origDestroy = Backbone.Model.prototype.destroy;
-  Backbone.Model.prototype.destroy = function destory(){
+  Backbone.Model.prototype.destroy = function destroy(){
     var xhr = origDestroy.apply(this, arguments);
     return (xhr !== false) ? xhr : Promise.reject(new Error('ModelError'));
   };
