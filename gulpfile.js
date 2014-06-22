818636acea3c0e88545bf6fd32cdc637ca938e4a
tim walker <tim.twalker@gmail.com>
2014-06-21 17:56:10 -0700

    some work on this gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp')
   , bump = require('gulp-bump')
   , clean = require('gulp-clean')
   , stylus = require('gulp-stylus')
+  , nib = require('nib')
   , notify = require('gulp-notify')
   , jshint = require('gulp-jshint')
   , rjs = require('gulp-requirejs')
@@ -12,18 +13,22 @@ var gulp = require('gulp')
 
 gulp.task('css', function(){
   return gulp.src('public/css/style.styl')
-    .pipe(stylus({use: ['nib']}))
+    //.pipe(watch())
+    .pipe(stylus({use: [nib()], import: ['nib']}))
     .pipe(gulp.dest('public/css'))
     .pipe(livereload(server))
-    .pipe(notify({ message: 'css built' }));
+    //.pipe(livereload())
+    //.pipe(notify({ message: 'css built' }));
 });
+
+
 
 gulp.task('js', function() {
   return gulp.src(['./public/js/src/**/*.js', '!./public/js/src/bower_components/**'])
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(rjs({
-      out: 'eggshell.dev.js',
+      out: 'eggshell.js',
       baseUrl: './public/js/src',
       mainConfigFile: './public/js/src/config.js',
       name: 'config',
@@ -31,14 +36,14 @@ gulp.task('js', function() {
       include: 'require-lib',
       useSourceUrl: true
     }))
-    .pipe(gulp.dest('./public/js/dist/'))
+    .pipe(gulp.dest('./public/js/dist/dev/'))
     .pipe(livereload(server))
     .pipe(notify({ onLast: true, message: 'js built' }));
 });
-/*
+
 gulp.task('rjs-dev', function() {
-  rjs({
-    out: 'eggshell.dev.js',
+  return rjs({
+    out: 'eggshell.js',
     baseUrl: './public/js/src',
     mainConfigFile: './public/js/src/config.js',
     name: 'config',
@@ -46,23 +51,14 @@ gulp.task('rjs-dev', function() {
     include: 'require-lib',
     useSourceUrl: true
   })
-  .pipe(gulp.dest('./public/js/dist/'))
-  .pipe(livereload(server));
+  .pipe(gulp.dest('./public/js/dist/dev/'))
 });
 
-gulp.task('rjs-prod', function() {
-  rjs({
-    out: 'eggshell.prod.js',
-    baseUrl: './public/js/src',
-    mainConfigFile: './public/js/src/config.js',
-    name: 'config',
-    optimize: 'none',
-    include: 'require-lib'
-  })
-  .pipe(uglify({ outSourceMap: false }))
-  .pipe(gulp.dest('./public/js/dist/'))
+gulp.task('ugly', ['rjs-dev'], function() {
+  gulp.src(['./public/js/dist/dev/eggshell.js'])
+  .pipe(uglify())
+  .pipe(gulp.dest('./public/js/dist/prod/'))
 });
-*/
 
 gulp.task('clean', function() {
   return gulp.src(['./public/js/dist/'], {read: false})
@@ -90,4 +86,8 @@ gulp.task('watch', function(){
 
 gulp.task('default', ['clean'], function(){
   gulp.start('css', 'js', 'bump');
+});
+
+gulp.task('build', ['clean'], function(){
+  gulp.start('css', 'js', 'ugly', 'bump');
 });
