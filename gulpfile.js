29b99d8a38396f11bad72a7a8d52adba02bd6c43
tim walker <tim.twalker@gmail.com>
2014-06-22 16:15:56 -0700

    streaming frustrations

--- gulpfile.js
+++ gulpfile.js
@@ -11,47 +11,37 @@ var gulp = require('gulp'),
   jshint = require('gulp-jshint'),
   rjs = require('gulp-requirejs'),
   uglify = require('gulp-uglify'),
-  livereload = require('gulp-livereload'),
-  lr = require('tiny-lr'),
-  server = lr();
+  livereload = require('gulp-livereload');
+
 
 gulp.task('css', function(){
   return gulp.src('public/css/style.styl')
     .pipe(stylus({use: [nib()], import: ['nib']}))
-    .pipe(gulp.dest('public/css'))
-    .pipe(livereload(server))
+    .pipe(gulp.dest('public/css'));
 });
 
-// gulp.task('js', function() {
-//   return gulp.src(['./public/js/src/**/*.js', '!./public/js/src/bower_components*'])
-//     .pipe(jshint('.jshintrc'))
-//     .pipe(jshint.reporter('jshint-stylish'))
-//     .pipe(rjs({
-//       out: 'eggshell.js',
-//       baseUrl: './public/js/src',
-//       mainConfigFile: './public/js/src/config.js',
-//       name: 'config',
-//       optimize: 'none',
-//       include: 'require-lib',
-//       useSourceUrl: true
-//     }))
-//     .pipe(gulp.dest('./public/js/dist/dev/'))
-//     .pipe(livereload(server))
-// });
-
-
-gulp.task('js', ['lint', 'rjs']);
-
-gulp.task('lint', function() {
+gulp.task('js', function() {
   return gulp.src(['./public/js/src/**/*.js', '!./public/js/src/bower_components/**'])
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(jshint.reporter('fail'));
+    .pipe(gulp.src(rjs({
+      out: 'eggshell.js',
+      baseUrl: './public/js/src',
+      mainConfigFile: './public/js/src/config.js',
+      name: 'config',
+      optimize: 'none',
+      include: 'require-lib',
+      useSourceUrl: true
+    }).pipe(gulp.dest('./public/js/dist/dev/')))
+    )
+
+    //.pipe(gulp.dest('./public/js/dist/dev/'))
+    //.pipe(uglify())
+    //.pipe(gulp.dest('./public/js/dist/prod/'));
 });
 
-
 gulp.task('rjs', ['lint'], function(){
-  rjs({
+  return rjs({
     out: 'eggshell.js',
     baseUrl: './public/js/src',
     mainConfigFile: './public/js/src/config.js',
@@ -61,38 +51,47 @@ gulp.task('rjs', ['lint'], function(){
     useSourceUrl: true
   })
   .pipe(gulp.dest('./public/js/dist/dev/'))
-  .pipe(livereload(server));
+  .pipe(uglify())
+  .pipe(gulp.dest('./public/js/dist/prod/'));
 });
 
-
-gulp.task('minify', ['js'], function() {
-  gulp.src(['./public/js/dist/dev/eggshell.js'])
-    .pipe(uglify())
-    .pipe(gulp.dest('./public/js/dist/prod/'))
+gulp.task('lint', function() {
+  return gulp.src(['./public/js/src/**/*.js', '!./public/js/src/bower_components/**'])
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('clean', function() {
-  return gulp.src(['./public/js/dist/'], {read: false})
+  return gulp.src(['./public/js/dist'], {read: false})
     .pipe(clean());
 });
 
 // Increment packages by patch point
 gulp.task('bump', function(){
-  gulp.src(['./bower.json', './package.json'])
+  return gulp.src(['./bower.json', './package.json'])
     .pipe(bump())
     .pipe(gulp.dest('./'));
 });
 
 gulp.task('watch', function(){
-  server.listen(35729, function(err) {
-    if (err) return console.log(err);
-    gulp.watch('./public/css/style.styl', ['css']);
-    gulp.watch('./public/js/src/**/*.js', ['js']);
+
+  gulp.watch('./public/css/style.styl', ['css']);
+  gulp.watch(['./public/js/src/**/*.js','public/js/src/views/**/*.mustache'], ['js']);
+
+  var server = livereload();
+  gulp.watch([
+    './public/js/dist/**/*.js',
+    './public/js/test/*.js',
+    './public/css/*.css'
+  ]).on('change', function(file) {
+    //console.log(file.path + ' changed')
+    server.changed(file.path);
   });
 
 });
 
-gulp.task('default', ['clean', 'css', 'js']);
+gulp.task('default', ['clean', 'css', 'lint', 'rjs']);
 
-gulp.task('build', ['clean', 'css', 'js', 'minify', 'bump']);
+gulp.task('build', ['clean', 'css', 'rjs', 'bump']);
 
