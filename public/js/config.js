d7e97d85db2a0bd4c4211182b7ca30df752510f3
tim walker <tim.twalker@gmail.com>
2014-10-25 16:33:34 -0700

    add jspm package.json

---
+++ public/js/config.js
@@ -0,0 +1,109 @@
+System.config({
+  "paths": {
+    "*": "*.js",
+    "npm:*": "jspm_packages/npm/*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "lodash": "npm:lodash-node@^2.4.1",
+    "mocha": "npm:mocha@^2.0.1",
+    "backbone": "npm:backbone@^1.1.2",
+    "github:jakearchibald/es6-promise": "github:jakearchibald/es6-promise@^1.0.0",
+    "github:chaijs/chai": "github:chaijs/chai@^1.9.2",
+    "github:cjohansen/Sinon.JS": "github:cjohansen/Sinon.JS@^1.10.3",
+    "text": "github:systemjs/plugin-text@^0.0.2",
+    "jquery": "github:components/jquery@^2.1.1",
+    "mustache": "github:janl/mustache.js@^0.8.2",
+    "npm:mocha@2.0.1": {
+      "diff": "npm:diff@1.0.8",
+      "commander": "npm:commander@2.3.0",
+      "debug": "npm:debug@2.0.0",
+      "growl": "npm:growl@1.8.1",
+      "jade": "npm:jade@0.26.3",
+      "escape-string-regexp": "npm:escape-string-regexp@1.0.2",
+      "glob": "npm:glob@3.2.3",
+      "mkdirp": "npm:mkdirp@0.5.0"
+    },
+    "npm:jade@0.26.3": {
+      "commander": "npm:commander@0.6.1",
+      "mkdirp": "npm:mkdirp@0.3.0"
+    },
+    "npm:backbone@1.1.2": {
+      "underscore": "pm:lodash-node@2.4.1"
+    },
+    "npm:debug@2.0.0": {
+      "ms": "npm:ms@0.6.2"
+    },
+    "npm:glob@3.2.3": {
+      "inherits": "npm:inherits@2",
+      "graceful-fs": "npm:graceful-fs@2.0",
+      "json": "npm:json@^9.0.1",
+      "minimatch": "npm:minimatch@0"
+    },
+    "npm:mkdirp@0.5.0": {
+      "minimist": "npm:minimist@0.0.8"
+    },
+    "npm:minimatch@0.4.0": {
+      "sigmund": "npm:sigmund@1.0",
+      "lru-cache": "npm:lru-cache@2"
+    },
+    "npm:lodash-node@2.4.1": {},
+    "npm:diff@1.0.8": {},
+    "npm:commander@2.3.0": {},
+    "npm:growl@1.8.1": {},
+    "npm:commander@0.6.1": {},
+    "npm:escape-string-regexp@1.0.2": {},
+    "npm:ms@0.6.2": {},
+    "npm:mkdirp@0.3.0": {},
+    "npm:inherits@2.0.1": {},
+    "npm:graceful-fs@2.0.3": {},
+    "npm:json@9.0.1": {},
+    "npm:minimist@0.0.8": {},
+    "npm:sigmund@1.0.0": {},
+    "npm:lru-cache@2.5.0": {},
+    "github:jspm/nodelibs@0.0.5": {
+      "inherits": "npm:inherits@^2.0.1"
+    }
+  }
+});
+
+System.config({
+  "versions": {
+    "npm:lodash-node": "2.4.1",
+    "npm:mocha": "2.0.1",
+    "npm:backbone": "1.1.2",
+    "npm:diff": "1.0.8",
+    "npm:commander": [
+      "2.3.0",
+      "0.6.1"
+    ],
+    "npm:debug": "2.0.0",
+    "npm:growl": "1.8.1",
+    "npm:jade": "0.26.3",
+    "github:jakearchibald/es6-promise": "1.0.0",
+    "npm:escape-string-regexp": "1.0.2",
+    "npm:ms": "0.6.2",
+    "npm:glob": "3.2.3",
+    "npm:mkdirp": [
+      "0.5.0",
+      "0.3.0"
+    ],
+    "github:chaijs/chai": "1.9.2",
+    "github:cjohansen/Sinon.JS": "1.10.3",
+    "github:systemjs/plugin-text": "0.0.2",
+    "npm:inherits": "2.0.1",
+    "npm:graceful-fs": "2.0.3",
+    "npm:json": "9.0.1",
+    "npm:minimatch": "0.4.0",
+    "npm:minimist": "0.0.8",
+    "npm:sigmund": "1.0.0",
+    "npm:lru-cache": "2.5.0",
+    "github:components/jquery": "2.1.1",
+    "github:jspm/nodelibs": "0.0.5",
+    "github:janl/mustache.js": "0.8.2"
+  }
+});
+
